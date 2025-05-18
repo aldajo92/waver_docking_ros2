635bfc718f26b2358c3e1244ca9ec784a834f557
Alejandro Gomez <alejandro.gomez.-nd@disney.com>
2025-05-18 05:12:10 -0500

    chore: added matrix conversion

--- src/docking_node.cpp
+++ src/docking_node.cpp
@@ -24,14 +24,22 @@ public:
     Q_.x() = this->get_parameter("Q.x").as_double();
     Q_.y() = this->get_parameter("Q.y").as_double();
 
-    // Create a 2D rotation matrix for 90 degrees (counterclockwise)
+    // Create a 2D rotation matrix for 90 degrees (PI/2 radians, counterclockwise)
+    double angle = M_PI / 2.0; // 90 degrees in radians
     Eigen::Matrix2d rot90;
-    rot90 << 0, -1,
-            1,  0;
+    rot90 << cos(angle), -sin(angle),
+         sin(angle),  cos(angle);
 
     PQMid_ = (P_ + Q_) / 2.0;
     e_X_ = (Q_ - PQMid_).normalized();
     e_Y_ = rot90 * e_X_;
+    e_Y_angle_ = vector_angle(e_Y_);
+
+    Eigen::Matrix2d matrix_basis;
+    matrix_basis.col(0) = e_X_;
+    matrix_basis.col(1) = e_Y_;
+
+    Eigen::Matrix2d matrix_basis_inv = matrix_basis.inverse();
 
     timer_ = this->create_wall_timer(
         std::chrono::milliseconds(100), // 10 Hz
@@ -55,16 +63,27 @@ private:
 
     RCLCPP_INFO(this->get_logger(), "P: (%.2f, %.2f), Q: (%.2f, %.2f), PQMid: (%.2f, %.2f)",
                 P_.x(), P_.y(), Q_.x(), Q_.y(), PQMid_.x(), PQMid_.y());
+    RCLCPP_INFO(this->get_logger(), "e_X: (%.2f, %.2f), e_Y: (%.2f, %.2f)",
+                e_X_.x(), e_X_.y(), e_Y_.x(), e_Y_.y());
+    RCLCPP_INFO(this->get_logger(), "e_Y_angle: %.2f", e_Y_angle_);
+    RCLCPP_INFO(this->get_logger(), "e_Y_angle in degrees: %.2f", e_Y_angle_ * 180.0 / M_PI);
     RCLCPP_INFO(this->get_logger(),
                 "\nDist \t Error: %.2f, Output: %.2f, Distance: %.2f \nAngle \t Error: %.2f, Output: %.2f, Angle: %.2f",
                 distance_error, distance_output, current_distance_,
                 angle_error, angle_output, current_angle_);
   }
 
+  double vector_angle(Eigen::Vector2d vector) const
+  {
+    // Calculate the angle in radians
+    return std::atan2(vector.y(), vector.x());
+  }
+
   waver_docking::PIDParameters distance_pid_params_;
   waver_docking::PIDParameters angle_pid_params_;
   Eigen::Vector2d P_, Q_, PQMid_;
   Eigen::Vector2d e_X_, e_Y_;
+  double e_Y_angle_ = 0.0;
 
   pid_module::PIDController distance_pid_;
   pid_module::PIDController angle_pid_;
